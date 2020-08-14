e76b2602cfae40cf40edc12f6334fc6eab954aa5
smogul <45224724+smogul@users.noreply.github.com>
2020-08-14 08:46:05 +0200

    Update WeatherServiceModel.cs

--- CIBApp/CIBApp/ServiceModels/WeatherServiceModel.cs
+++ CIBApp/CIBApp/ServiceModels/WeatherServiceModel.cs
@@ -8,6 +8,6 @@ namespace CIBApp.ServiceModels
     {
         public static string WeatherApiAddress = "api.openweathermap.org/data/2.5/";
 
-        public static string WeatherApiKey = "9b9cde3687969313d6a94e0d07de1061";
+        public static string WeatherApiKey = "YOUR KEY";
     }
 }
